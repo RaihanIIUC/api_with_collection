b23068d8bba1211f9b6652505db2bab1dcbc4bb8
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2021-10-14 09:19:52 +0600

    initialization

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use Auth;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //  $movies = Auth::user()->movies;
+        // $movies =
+        // Movie::with('ratings', 'category')
+        //     ->withAvg('ratings', 'rating')
+        //     ->orderByDesc('ratings_avg_rating')
+        //     ->take(100)
+        //     ->get();
+
+        $movies = Movie::all()->sortByDesc(function ($movie) {
+            return $movie->ratings->avg('rating');
+        })->take(100);
+
+        
+        return response()->json([
+            'success' => true,
+            'data' => $movies
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $movie)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RatingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+ 
+Route::middleware('auth:api')->group(function () {
+    Route::resource('posts', PostController::class);
+    Route::resource('movies', MovieController::class);
+    Route::resource('categorys', CategoryController::class);
+    Route::resource('ratings', RatingController::class);
+});
